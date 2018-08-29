a7d518e16c6c1be210deec0bb1bc8b737660aff1
StanleyHuang <s703702002@gmail.com>
2018-08-29 19:33:09 +0800

    update

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 import "babel-polyfill"
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-
-import Counter from './Counter'
+import Counter from './components/Counter'
 import reducer from './reducers'
-
 import rootSaga from './sagas'
 
 const sagaMiddleware = createSagaMiddleware();
@@ -26,7 +23,7 @@ function render() {
       onIncrement={() => action('INCREMENT')}
       onDecrement={() => action('DECREMENT')}
       onIncrementAsync={() => action('INCREMENT_ASYNC')}
-      onHello={() => action('HELLO_SAGA')} />,
+    />,
     document.getElementById('root')
   )
 }
--- sagas/index.js
+++ sagas/index.js
@@ -1,19 +1,12 @@
 import { delay } from 'redux-saga'
 import { put, takeEvery, all, call } from 'redux-saga/effects'
 
-export function* helloSaga(){
-    console.log('hello saga! 我會再3秒後 + 1');
-    yield call(delay, 3000)
-    yield put({type: 'INCREMENT'})
-}
-
 export function* incrementAsync() {
     yield call(delay, 1000)
     yield put({type: 'INCREMENT'})
 }
 
 export default function* watchIncrementAsync() {
-    yield takeEvery('HELLO_SAGA', helloSaga)
     yield takeEvery('INCREMENT_ASYNC', incrementAsync)
 }
 
