5d1b070f031c7849da57a15569aa8676061e65dc
StanleyHuang <s703702002@gmail.com>
2018-08-29 18:37:43 +0800

    測試ssr

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,12 @@
 export default function rootReducer(state = 0, action) {
     switch (action.type) {
         case 'INCREMENT':
-          return state + 1
+            return state + 1
+        case 'INCREMENT_IF_ODD':
+            return (state % 2 !== 0) ? state + 1 : state
         case 'DECREMENT':
-          return state - 1
+            return state - 1
         default:
-          return state
+            return state
       }
 }
